45b7bc813a32e4df8143f1e6f0257f7556ee4205
Cogiii <laurencedevera8@gmail.com>
2026-03-14 00:20:52 +0800

    fix: error in streamlit

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -2,11 +2,9 @@
 from .sidebar import render_sidebar
 from .metrics import render_metric_card, render_risk_metrics
 from .tables import render_risk_table, render_stock_table
-from .charts import render_bar_chart, render_line_chart
 
 __all__ = [
     "render_sidebar",
     "render_metric_card", "render_risk_metrics",
-    "render_risk_table", "render_stock_table",
-    "render_bar_chart", "render_line_chart"
+    "render_risk_table", "render_stock_table"
 ]
--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,7 +1,6 @@
 """Page modules."""
 from .risk_dashboard import render_risk_dashboard
 from .stock_overview import render_stock_overview
-from .analytics import render_analytics
 from .predict import render_predict
 
-__all__ = ["render_risk_dashboard", "render_stock_overview", "render_analytics", "render_predict"]
+__all__ = ["render_risk_dashboard", "render_stock_overview", "render_predict"]
